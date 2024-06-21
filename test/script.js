74f94550a9698f60f151d1fd4e8a232a1900a2b9
Justin BRUNEAU <justin.bruneau@progial.fr>
2024-06-21 11:56:36 +0200

    push

--- test/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.cta-button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert(`${button.textContent} a été cliqué !`);
-        });
-    });
-});
